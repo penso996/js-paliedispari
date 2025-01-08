db60487f52a518057b005a1f10c2df98eadf432a
penso996 <penso.andrea@outlook.it>
2025-01-08 17:28:32 +0100

    added comments and lightened code

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,12 +1,14 @@
-let chosenWord = prompt("Inserisci una parola.");
+//Chiedi all'utente di inserire una parola
+let inputWord = prompt("Inserisci una parola.");
 
-let palindrome = isPalindrome(chosenWord);
-if (palindrome) {
+//Usa il risultato della funzione creata in modo che il valore booleano dia un messaggio personalizzato all'utente
+if (isPalindrome(inputWord)) {
     console.log("La parola che hai inserito è un palindromo.");
 } else {
     console.log("La parola che hai inserito NON è un palindromo.");
 }
 
+//Crea la funzione che verifica se la parola inserita sia un palindromo o meno
 function isPalindrome(word) {
     let normalizedWord = word.toLowerCase();
 
